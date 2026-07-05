0d367208d8ccbd24e9e2793b676972ae532ba778
Abhijeet Singh Pawar <114191678+aspabhi31@users.noreply.github.com>
2026-07-05 14:29:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,15 @@ uploaded_file = st.file_uploader("Upload the file", type=["jpg", "png"])
 import tempfile
 import os
 
-# Get the original extension (e.g., .png, .jpg)
-ext = os.path.splitext(uploaded_file.name)[1]  # includes the dot
-with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as tmp:
-    tmp.write(uploaded_file.getbuffer())
-    image_path = tmp.name
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
-prediction = predict(image_path)
+if uploaded_file is not None:
+    ext = os.path.splitext(uploaded_file.name)[1]
+    with tempfile.NamedTemporaryFile(delete=False, suffix=ext) as tmp:
+        tmp.write(uploaded_file.getbuffer())
+        image_path = tmp.name
+    prediction = predict(image_path)
+    st.image(uploaded_file, caption="Uploaded File", width='stretch')
+    st.info(f"Predicted Class: {prediction}")
+else:
+    st.write("Please upload an image file.")
